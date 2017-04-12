7506a8f928cd70aef3f1f966a2d5715650638374
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2017-04-11 22:50:14 -0600

    crud routes working

--- src/server/routes/favorites.js
+++ src/server/routes/favorites.js
@@ -8,23 +8,34 @@ const indexController = require('../controllers/index');
 
 router.get('/', (req, res, next) => {
   knex('airports').select('*')
-    .then((response) => {
-      res.json(response)
-    })
-});
+  .then((response) => {
+    res.json(response)
+  })
+})
 
 router.post('/', (req, res, next) => {
-  console.log(req.body)
   knex('airports').insert({
     name: req.body.name,
     code: req.body.code
   })
-    .then((response) => {
-      res.json(response)
-    })
-    .catch((error) => {
-      res.status(500).send(error)
-    })
-});
+  .then((response) => {
+    res.json(response)
+  })
+  .catch((error) => {
+    res.status(500).send(error)
+  })
+})
+
+router.delete('/:code', (req, res, next) => {
+  console.log(req.params.code)
+  knex('airports')
+  .where({
+    code: req.params.code
+  })
+  .del()
+  .then((response) => {
+    res.json(response)
+  })
+})
 
 module.exports = router;
